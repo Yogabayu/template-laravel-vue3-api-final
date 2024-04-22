00627cb50d5eed9003cff94fcb6e772ba63ef423
Yogabayu <yogabayusbi@gmail.com>
2024-04-22 16:56:02 +0700

    ref: refactor code

--- resources/js/pages/user/dashboard.vue
+++ resources/js/pages/user/dashboard.vue
@@ -43,6 +43,8 @@ export default {
       fileRandom: null,
       fileFav: null,
       newestFile: {},
+      
+      userData:{},
     };
   },
   methods: {
@@ -53,15 +55,14 @@ export default {
     // toDetailFile(item) {
     //   this.$router.push(`/u-filedetail/${item}`);
     // },
-    // getUserDataAndToken() {
-    //   const savedUserData = localStorage.getItem("userData");
-    //   const savedUserToken = localStorage.getItem("userToken");
+    getUserData() {
+      const savedUserData = localStorage.getItem("userData");
 
-    //   if (savedUserData && savedUserToken) {
-    //     this.userData = JSON.parse(savedUserData);
-    //     this.userToken = savedUserToken;
-    //   }
-    // },
+      if (savedUserData) {
+        this.userData = JSON.parse(savedUserData);
+      }
+      console.log(this.userData);
+    },
     // async getTotalFileAvailable() {
     //   try {
     //     const response = await mainURL.get("/user/total-file");
@@ -83,109 +84,9 @@ export default {
     //     this.$showToast("error", "Sorry", errorMessage);
     //   }
     // },
-    // async getTotalFileRead() {
-    //   try {
-    //     const response = await mainURL.get("/user/total-read");
-
-    //     if (response.status === 200) {
-    //       this.tRead = response.data.data;
-    //     } else {
-    //       const errorMessage =
-    //         response && response.data && response.data.message
-    //           ? response.data.message
-    //           : "Gagal. Silakan coba lagi.";
-    //       this.$showToast("error", "Sorry", errorMessage);
-    //     }
-    //   } catch (error) {
-    //     const errorMessage =
-    //       error.response && error.response.data && error.response.data.message
-    //         ? error.response.data.message
-    //         : "Gagal login. Silakan coba lagi.";
-    //     this.$showToast("error", "Sorry", errorMessage);
-    //   }
-    // },
-    // async getTotalFileFav() {
-    //   try {
-    //     const response = await mainURL.get("/user/total-fav");
-
-    //     if (response.status === 200) {
-    //       this.tFav = response.data.data;
-    //     } else {
-    //       const errorMessage =
-    //         response && response.data && response.data.message
-    //           ? response.data.message
-    //           : "Gagal. Silakan coba lagi.";
-    //       this.$showToast("error", "Sorry", errorMessage);
-    //     }
-    //   } catch (error) {
-    //     const errorMessage =
-    //       error.response && error.response.data && error.response.data.message
-    //         ? error.response.data.message
-    //         : "Gagal login. Silakan coba lagi.";
-    //     this.$showToast("error", "Sorry", errorMessage);
-    //   }
-    // },
-    // async getDashboard() {
-    //   try {
-    //     const response = await mainURL.get("/user/index");
-
-    //     if (response.status === 200) {
-    //       this.fileRandom = response.data.data;
-    //     } else {
-    //       const errorMessage =
-    //         response && response.data && response.data.message
-    //           ? response.data.message
-    //           : "Gagal. Silakan coba lagi.";
-    //       this.$showToast("error", "Sorry", errorMessage);
-    //     }
-
-    //     const response2 = await mainURL.get("/user/fileFav");
-
-    //     if (response2.status === 200) {
-    //       this.fileFav = response2.data.data;
-    //     } else {
-    //       const errorMessage =
-    //         response2 && response2.data && response2.data.message
-    //           ? response2.data.message
-    //           : "Gagal. Silakan coba lagi.";
-    //       this.$showToast("error", "Sorry", errorMessage);
-    //     }
-    //   } catch (error) {
-    //     const errorMessage =
-    //       error.response && error.response.data && error.response.data.message
-    //         ? error.response.data.message
-    //         : "Gagal login. Silakan coba lagi.";
-    //     this.$showToast("error", "Sorry", errorMessage);
-    //   }
-    // },
-    // async getNewestFile() {
-    //   try {
-    //     const response = await mainURL.get("/user/newestfile");
-
-    //     if (response.status === 200) {
-    //       this.newestFile = response.data.data;
-    //     } else {
-    //       const errorMessage =
-    //         response && response.data && response.data.message
-    //           ? response.data.message
-    //           : "Gagal. Silakan coba lagi.";
-    //       this.$showToast("error", "Sorry", errorMessage);
-    //     }
-    //   } catch (error) {
-    //     const errorMessage =
-    //       error.response && error.response.data && error.response.data.message
-    //         ? error.response.data.message
-    //         : "Gagal login. Silakan coba lagi.";
-    //     this.$showToast("error", "Sorry", errorMessage);
-    //   }
-    // }
   },
   mounted() {
-    // this.getTotalFileAvailable();
-    // this.getTotalFileRead();
-    // this.getTotalFileFav();
-    // this.getDashboard();
-    // this.getNewestFile();
+    this.getUserData();
   },
 };
 </script>
