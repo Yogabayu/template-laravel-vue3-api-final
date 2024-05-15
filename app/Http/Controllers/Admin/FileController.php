6f2dc3e3d441d758ab8c0f22b88eed84c56483ea
Yogabayu <yogabayusbi@gmail.com>
2024-05-15 12:05:48 +0700

    prod: update bug

--- app/Http/Controllers/Admin/FileController.php
+++ app/Http/Controllers/Admin/FileController.php
@@ -816,105 +816,17 @@ class FileController extends Controller
     public function index()
     {
         try {
-            $userNow = User::with('position')->where('id', Auth::user()->id)->first();
+            $userAccess = [
+                'canAppeal' => 1,
+                'canApprove' => 1,
+                'canInsertData' => 1,
+                'isSecret' => 1,
+            ];
+            // Inisialisasi array untuk menampung semua file yang terkait
+            $files = File::all();
+            ActivityHelper::userActivity(Auth::user()->id, 'Mengakses halaman File Credit');
 
-            if ($userNow->position->name == 'Account Officer' || $userNow->position->name == 'AO' || $userNow->position->name == 'ao' || $userNow->position->name == 'account officer' || $userNow->position->name == 'Account Officer Exceutive') {
-
-                $positionId = Auth::user()->position_id;
-                $position = Position::with('offices')->where('id', $positionId)->first();
-
-                // Dapatkan semua kantor yang terkait dengan posisi
-                $offices = $position->offices;
-
-                // dd($offices);
-                //get list access for user
-                $notifPositions = [];
-                foreach ($offices as $office) {
-                    $notificationConfigurations = DB::table('notification_configurations')
-                        ->where('office_id', $office->id)
-                        ->where('phase', 1)
-                        ->get();
-
-                    $notifPositions = array_merge($notifPositions, $notificationConfigurations->toArray());
-                }
-                $userPosNow = User::where('id', Auth::user()->id)->first();
-                $userAccess = [];
-
-                foreach ($notifPositions as $notifPosition) {
-                    if ($notifPosition->position_id == $userPosNow->position_id) {
-                        $userAccess = [
-                            'canAppeal' => $notifPosition->canAppeal,
-                            'canApprove' => $notifPosition->canApprove,
-                            'canInsertData' => $notifPosition->canInsertData,
-                            'isSecret' => $notifPosition->isSecret,
-                        ];
-                        break;
-                    }
-                }
-                $files = File::where('user_id', Auth::user()->id)->get();
-
-                ActivityHelper::userActivity(Auth::user()->id, 'Mengakses halaman File Credit');
-
-                return ResponseHelper::successRes('Berhasil menampilkan datas', ['files' => $files, 'userAccess' => $userAccess]);
-            } else {
-                $positionId = Auth::user()->position_id;
-                $position = Position::with('offices')->where('id', $positionId)->first();
-
-                // Dapatkan semua kantor yang terkait dengan posisi
-                $offices = $position->offices;
-
-                // dd($offices);
-                //get list access for user
-                $notifPositions = [];
-                foreach ($offices as $office) {
-                    $notificationConfigurations = DB::table('notification_configurations')
-                        ->where('office_id', $office->id)
-                        ->where('phase', 1)
-                        ->get();
-
-                    $notifPositions = array_merge($notifPositions, $notificationConfigurations->toArray());
-                }
-                $userPosNow = User::where('id', Auth::user()->id)->first();
-                $userAccess = [];
-
-                foreach ($notifPositions as $notifPosition) {
-                    if ($notifPosition->position_id == $userPosNow->position_id) {
-                        $userAccess = [
-                            'canAppeal' => $notifPosition->canAppeal,
-                            'canApprove' => $notifPosition->canApprove,
-                            'canInsertData' => $notifPosition->canInsertData,
-                            'isSecret' => $notifPosition->isSecret,
-                        ];
-                        break;
-                    }
-                }
-
-                // Inisialisasi array untuk menampung semua file yang terkait
-                $files = [];
-
-                $fileAll = File::all();
-                foreach ($fileAll as $eachFile) {
-                    // Periksa posisi pengguna yang mengunggah file
-                    $uploaderPositionId = DB::table('users')
-                        ->where('id', $eachFile->user_id)
-                        ->value('position_id');
-
-                    // Dapatkan daftar kantor yang terkait dengan posisi pengguna
-                    $uploaderOfficeIds = DB::table('positiontooffices')
-                        ->where('position_id', $uploaderPositionId)
-                        ->pluck('office_id')
-                        ->toArray();
-
-                    // Periksa apakah ada kantor yang sama antara kantor pengguna dan kantor-kantor yang terkait dengan posisi yang dicari
-                    if (array_intersect($uploaderOfficeIds, $offices->pluck('id')->toArray())) {
-                        // Jika ada, tambahkan file ke dalam array files
-                        $files[] = $eachFile;
-                    }
-                }
-                ActivityHelper::userActivity(Auth::user()->id, 'Mengakses halaman File Credit');
-
-                return ResponseHelper::successRes('Berhasil menampilkan datas', ['files' => $files, 'userAccess' => $userAccess]);
-            }
+            return ResponseHelper::successRes('Berhasil menampilkan datas', ['files' => $files, 'userAccess' => $userAccess]);
         } catch (\Exception $e) {
             return ResponseHelper::errorRes($e->getMessage());
         }
