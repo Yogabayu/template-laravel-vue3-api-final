0a7bb6f2ed7a8afbccbee96ccc6907b54fd40c0d
Yogabayu <yogabayusbi@gmail.com>
2024-06-12 13:05:38 +0700

    udpate

--- app/Http/Controllers/Admin/FileController.php
+++ app/Http/Controllers/Admin/FileController.php
@@ -1630,14 +1630,14 @@ class FileController extends Controller
                 ['approved' => 0]
             );
 
-            EmailHelper::AddUpdate($file->id);
-            TelegramHelper::AddFile($file->id);
-
             //add count time
             PhaseTime::firstOrCreate(['file_id' => $file->id, 'phase' => $file->phase, 'startTime' => Carbon::now()]);
 
             ActivityHelper::fileActivity($file->id, Auth::user()->id, 'Menambahkan Data Kredit');
             ActivityHelper::userActivity(Auth::user()->id, 'Menambahkan Data Kredit' . $file->name);
+
+            EmailHelper::AddUpdate($file->id);
+            TelegramHelper::AddFile($file->id);
 
             return ResponseHelper::successRes('Berhasil menambahkan data', $file);
         } catch (\Exception $e) {
